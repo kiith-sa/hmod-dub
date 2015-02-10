050e45c7e04d4da126a3275a251d996d47f9d30e
Ferdinand Majerech <kiithsacmp@gmail.com>
2015-02-10 22:52:03 +0100

    License

--- source/app.d
+++ source/app.d
@@ -1,3 +1,8 @@
+//          Copyright Ferdinand Majerech 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 import std.algorithm;
 import std.array: empty, popFront, front, back;
 import std.conv: to;
